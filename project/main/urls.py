c3186aed493e596ab9c061839496de19d9c53373
Ahmed El Gendy <elgendyahmed753@gmail.com>
2024-10-31 16:44:26 +0300

    Add Courses To the user

--- project/main/urls.py
+++ project/main/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import adminhome, userhome, login, index, custom_logout, c1, c2, c3, done1, done2, done3, userdata, course_detail, done, course_search
+from .views import adminhome, userhome, login, index, custom_logout, userdata, course_detail, done, course_search, course_table, save_user_courses
 from django.contrib.auth.views import LogoutView
 from django.contrib.auth import views as auth_views
 
@@ -10,6 +10,8 @@ urlpatterns = [
     path('', userhome, name='userhome'),
     path('userdata/<int:user_id>/', userdata, name='userdata'),
     path('course/<int:id>/', course_detail, name='course_detail'),
+    path('course_table/', course_table, name='course_table'),
+    path('save-user-courses/<int:id>/', save_user_courses, name='save_user_courses'),
     path('done/<int:id>/', done, name='done'),
     path('manger/', index, name='index'),
     path('adminhome/', adminhome, name='adminhome'),
