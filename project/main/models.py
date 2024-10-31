2153b032486755f7b74146634974bde129cd4f5b
Ramyrashad127 <ramy41599@gmail.com>
2024-10-31 13:16:05 +0300

    edit templates

--- project/main/models.py
+++ project/main/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Course(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique=True)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='images/', blank=True, null=True)
@@ -13,14 +13,23 @@ class Course(models.Model):
         return self.title
 
 
+from django.db import models
+from django.contrib.auth.models import User
+
 class UserCourse(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     done = models.BooleanField(default=False)
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'course'], name='unique_user_course')
+        ]
+
     def __str__(self):
         return f"{self.user.username} enrolled in {self.course.title}"
+
 
 
 class Admin(models.Model):
