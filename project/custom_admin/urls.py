a3a603fe970af3bdbb486ebd41a0e279315556d8
Ramyrashad127 <ramy41599@gmail.com>
2024-11-02 21:42:26 +0200

    Add make_user_admin view to promote users to admin role

--- project/custom_admin/urls.py
+++ project/custom_admin/urls.py
@@ -4,10 +4,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.views import LogoutView
 from django.contrib.auth import views as auth_views
-from .views import adminsite, manage_courses, manage_usercourses
+from .views import adminsite, manage_courses, manage_usercourses, manage_users, add_user , edit_course, delete_course , edit_user, delete_user, make_user_admin
 
 urlpatterns = [
     path('', adminsite, name='adminsite'),
     path('manage-courses/', manage_courses, name='manage_courses'),
     path('manage-usercourses/', manage_usercourses, name='manage_usercourses'),
+    path('manage-users/', manage_users, name='manage_users'),
+    path('add-user/', add_user, name='add_user'),
+    path('edit-course/<int:course_id>/', edit_course, name='edit_course'),
+    path('delete-course/<int:course_id>/', delete_course, name='delete_course'),
+    path('edit-user/<int:user_id>/', edit_user, name='edit_user'),
+     path('delete-user/<int:user_id>/', delete_user, name='delete_user'),
+     path('make-admin/<int:user_id>/', make_user_admin, name='make_user_admin'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- project/custom_admin/views.py
+++ project/custom_admin/views.py
@@ -4,6 +4,10 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import CourseForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.forms import UserChangeForm
 
 
 @login_required
@@ -15,7 +19,7 @@ def add_course(request):
             return redirect('manage_courses')
     else:
         form = CourseForm()
-    return render(request, 'admin/add_course.html', {'form': form})
+    return render(request, 'add_course.html', {'form': form})
 
 @login_required
 def adminsite(request):
@@ -24,10 +28,79 @@ def adminsite(request):
 @login_required
 def manage_courses(request):
     courses = Course.objects.all()
-    return render(request, 'manage_courses.html', {'courses': courses})
+    return render(request, 'manage_course.html', {'courses': courses})
 
 @login_required
 def manage_usercourses(request):
     user_courses = UserCourse.objects.all()
     return render(request, 'manage_usercourses.html', {'user_courses': user_courses})
 
+@login_required
+def manage_users(request):
+    users = User.objects.all()
+    return render(request, 'manage_users.html', {'users': users})
+
+@login_required
+def add_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('manage_users')
+    else:
+        form = UserCreationForm()
+    return render(request, 'add_user.html', {'form': form})
+
+@login_required
+def edit_course(request, course_id):
+    course = get_object_or_404(Course, id=course_id)
+    if request.method == 'POST':
+        form = CourseForm(request.POST, instance=course)
+        if form.is_valid():
+            form.save()
+            return redirect('manage_courses')
+    else:
+        form = CourseForm(instance=course)
+    return render(request, 'edit_course.html', {'form': form, 'course': course})
+
+@login_required
+def delete_course(request, course_id):
+    course = get_object_or_404(Course, id=course_id)
+    if request.method == 'POST':
+        course.delete()
+        return redirect('manage_courses')
+    return render(request, 'delete_course.html', {'course': course})
+
+@login_required
+def edit_user(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('manage_users')
+    else:
+        form = UserChangeForm(instance=user)
+    return render(request, 'edit_user.html', {'form': form, 'user': user})
+
+@login_required
+def delete_user(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    if request.method == 'POST':
+        user.delete()
+        return redirect('manage_users')
+    return render(request, 'delete_user.html', {'user': user})
+
+@login_required
+def make_user_admin(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    if request.method == 'POST':
+        if not Admin.objects.filter(user=user).exists():
+            Admin.objects.create(user=user)
+            return redirect('manage_users')
+        else:
+            return render(request, 'make_admin.html', {
+                'user': user,
+                'error': 'This user is already an admin.'
+            })
+    return render(request, 'make_admin.html', {'user': user})
